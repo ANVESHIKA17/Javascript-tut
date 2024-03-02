6ecebb6aa9ed52bf9e97c1cee03f3bbb8e9aea70
ANVESHIKA17 <87799213+ANVESHIKA17@users.noreply.github.com>
2024-03-02 14:09:53 +0000

    string to number conversion

--- 01_basics/03_basics.js
+++ 01_basics/03_basics.js
@@ -5,3 +5,23 @@ console.log(typeof num)
 let isLoggedin=1
 let booleanIsloggein= Boolean(isLoggedin)
 console.log(booleanIsloggein)
+//operations
+let myval=3
+let negval=-myval
+console.log(negval)
+
+let str1="Anveshika"
+let str2=" Sharma"
+let str3=str1+str2
+// console.log(str3)
+// console.log("1"+2);
+// console.log(1+"2")
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+let num1,num2,num3
+num1=num2=num3=2+2
+console.table([num1,num2,num3])
+let gameCounter=100
+gameCounter++
+++gameCounter
+console.log(gameCounter)
