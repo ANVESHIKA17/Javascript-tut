4599ad2a79adb68cd20bb765307bd3ba099c9942
ANVESHIKA17 <87799213+ANVESHIKA17@users.noreply.github.com>
2024-03-02 13:02:17 +0000

    datatypes

---
+++ 01_basics/02_basics.js
@@ -0,0 +1,9 @@
+const password="123455"
+let city="banglore"
+var email="anv@gmail.com"
+let accountState;
+let abc=null; //datatype of null is object
+console.table([password,city,email,accountState])
+console.log(typeof abc)
+//const value cannot be changed
+//prefer do not use var due to issue in block scope and functional scope
