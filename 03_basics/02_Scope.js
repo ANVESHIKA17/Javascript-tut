eb52783771ba6ea08ba19cd6c6e88636abce22da
ANVESHIKA17 <87799213+ANVESHIKA17@users.noreply.github.com>
2024-03-08 18:53:41 +0000

    scopes

---
+++ 03_basics/02_Scope.js
@@ -0,0 +1,58 @@
+//var c = 300
+let a = 300
+if (true) {
+    let a = 10
+    const b = 20
+    // console.log("INNER: ", a);
+    
+}
+
+
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+function one(){
+    const username = "hitesh"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+     two()
+
+}
+
+// one()
+
+if (true) {
+    const username = "hitesh"
+    if (username === "hitesh") {
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+
+// ++++++++++++++++++ interesting ++++++++++++++++++
+
+// callinbg func before is allowed in this case as we dont store fun in a variable
+console.log(addone(5))
+
+function addone(num){
+    return num + 1
+}
+
+
+// storing a func inside a variable , hence not allowed to call func before fun declaration
+// addTwo(5)
+const addTwo = function(num){
+    return num + 2
+}
